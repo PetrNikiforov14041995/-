d5a4fd8b0412dd582c14fdae045a24d6e93809e3
Petr14 <npetya69@gmail.com>
2020-12-19 10:11:27 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import json
+import hashlib
 
-
+# чтение и запись в файл страна - ссылка
 class MyIterator:
-    with open('countries.json', 'r') as file:
-        text = json.load(file)
-        for key in text:
-            print(key)
+    with open("websites.txt", "w") as file:
+        with open('countries.json', 'r') as load:
+            text = json.load(load)
+            for key in text:
+                file.write(f"{key['name']['common']} - https://ru.wikipedia.org/wiki/{key['name']['common']}\n")
 
-    # def __init__(self, start, end):
-    #     self.start = start - 1
-    #     self.end = end
-    #
-    # def __iter__(self):
-    #     return self
-    #
-    # def __next__(self):
-    #     self.start += 1
-    #     if self.start == self.end:
-    #         raise StopIteration
-    #     return self.start
+
+# возврат md5 hash
+def MD5_hash(string, encoding='utf-8'):
+    md5_hasher = hashlib.md5()
+    md5_hasher.update(string.encode(encoding))
+    return md5_hasher.hexdigest()
+
+
+with open("websites.txt", "r") as f:
+    for line in f:
+        print(MD5_hash(line))
